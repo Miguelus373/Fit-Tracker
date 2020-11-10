6d3bb3a22512312016f5211e0ef9526a2242a69f
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-11-10 16:40:17 -0400

    Add main tracker and final styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def time_tracker(track)
+    raw("
+    <p class='main-tracker'>Total time tracked: <strong>#{to_time(track.total_time)}</strong></p>
+    ")
+  end
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,9 +1,11 @@
 class Exercise < ApplicationRecord
   validates :Name, presence: true
+  validates :time, presence: true
   validates :Amount, :distance, :time, numericality: { greater_than: 0, allow_nil: true }
 
   scope :grouped, -> { joins(:categories) }
   scope :not_grouped, -> { left_outer_joins(:categories).where('categories.group_id' => nil ) }
+  scope :total_time, -> { sum(:time) }
 
   belongs_to :user, class_name: 'User', foreign_key: :AuthorId
   has_many :categories
