9d6890465fe99675df2fac899b969a40b82f4dc6
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-11-12 11:29:33 -0400

    Remove duplicated column in groups table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_162314) do
+ActiveRecord::Schema.define(version: 2020_11_12_152316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2020_11_10_162314) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "icon"
   end
 
   create_table "users", force: :cascade do |t|
